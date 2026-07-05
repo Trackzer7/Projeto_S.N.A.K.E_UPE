66accd9868dc23cfcad7086b818a065f06c5f4b4
Trackzer <Trackzer7@users.noreply.github.com>
2026-07-04 23:22:27 -0300

    jogo base "finalizado"

--- S.N.A.K.E/cobra.py
+++ S.N.A.K.E/cobra.py
@@ -1,4 +1,5 @@
 import random
+
 def criar_cobra():
 # posicao inicial da cobra
    cobra = [[300,300],[300,300],[300,300]]
@@ -19,7 +20,8 @@ def movimento_cobra(cobra,comida,direcao,tamanhoCobra):
 def criar_comida(tamanhoCobra):
     comida = [random.randint(0,29)*tamanhoCobra,random.randint(0,29)*tamanhoCobra]
     return comida
-def detc_colisao (cobra):
+
+def detc_colisao (cobra,Barreiras):
     colisao = False
     if cobra[-1][0] < 0 or cobra[-1][0] >= 600:
         colisao = True
@@ -27,4 +29,33 @@ def detc_colisao (cobra):
         colisao = True
     if cobra[-1] in cobra[:-1]:
         colisao = True
+    if cobra[-1] in Barreiras:
+        colisao = True
     return colisao
+
+def criar_barreiras(dif,cobra,comida,tamanhoCobra):
+    quant = 0
+    if dif == 1:
+        quant = 0
+    if dif == 2:
+        quant = 15
+    if dif == 3:
+        quant = 25
+    Barreiras = []
+    for i in range(quant):
+        while True:
+            barreira = [random.randint(0,29)*tamanhoCobra,random.randint(0,29)*tamanhoCobra]
+            if barreira not in cobra and barreira != comida:
+                break
+        Barreiras.append(barreira)
+    return Barreiras
+
+def adicionar_barreiras(quantidade, cobra, comida, Barreiras, tamanhoCobra):
+    nova = []
+    for i in range(quantidade):
+        while True:
+            barreira = [random.randint(0,29)*tamanhoCobra,random.randint(0,29)*tamanhoCobra]
+            if barreira not in cobra and barreira != comida and barreira not in Barreiras and barreira not in nova:
+                break
+        nova.append(barreira)
+    return nova
--- S.N.A.K.E/main.py
+++ S.N.A.K.E/main.py
@@ -1,12 +1,13 @@
 import pygame
 import random
-from cobra import criar_cobra, movimento_cobra, criar_comida, detc_colisao
+from cobra import criar_cobra, movimento_cobra, criar_comida, detc_colisao, criar_barreiras, adicionar_barreiras
 from ranking import salvar_pontos
 
 
 pygame.init()
 tela = pygame.display.set_mode((600,600))
 tempo = pygame.time.Clock()
+velocidade = 10
 #cadastro de novo jogador
 nick = ''
 rodando = True
@@ -18,9 +19,12 @@ tamanhoCobra = 20
 direcao = [20,0]
 comida = criar_comida(tamanhoCobra)
 pontuacao = 0
+ultimo100 = 0
+#facil = 1, medio = 2, dificil = 3
+dif = None
 #tamanho da fonte da pontuacao
 fonte = pygame.font.SysFont(None, 36)
-
+#loop da tela de nick
 while inserindo_nick:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,11 +41,52 @@ while inserindo_nick:
                    nick += carac
     tela.fill((0,0,0))
     texto = fonte.render(f'Digite seu nick: {nick}', True, (255,255,255))
+    textoconfirma = fonte.render(f'ENTER para confirmar', True, (255,255,255))
     texto_rect = texto.get_rect(center=(300, 300))
+    
+    tela.blit(textoconfirma, (200,350) )
     tela.blit(texto, texto_rect)
     pygame.display.flip()
     tempo.tick(60)
+#loop tela de selecao dificuldade
+selec_dif = True
+if rodando == False:
+    selec_dif = False
+while selec_dif:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rodando = False
+            selec_dif = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                dif = 1
+                velocidade = 10
+                selec_dif = False
+            if event.key == pygame.K_2:
+                dif = 2
+                velocidade = 10
+                selec_dif = False
+            if event.key == pygame.K_3:
+                dif = 3
+                velocidade = 20
+                selec_dif = False
+    tela.fill((0,0,0))
+    texto = fonte.render(f'ESCOLHA A DIFICULDADE:', True, (0,255,0))
+    texto2 = fonte.render(f'1.FACIL', True, (0,255,100))
+    texto3 = fonte.render(f'2.MEDIO', True, (255,255,0))
+    texto4 = fonte.render(f'3.DIFICIL', True, (255,50,50))
 
+    tela.blit(texto, (150,200))
+    tela.blit(texto2, (250, 250))
+    tela.blit(texto3, (250, 300))
+    tela.blit(texto4, (250,350))
+    
+    pygame.display.flip()
+    tempo.tick(60)
+if rodando == True:
+    Barreiras = criar_barreiras(dif,cobra,comida,tamanhoCobra)
+else:
+    Barreiras = []
 #loop do jogo
 while rodando:
     nova_direcao = None
@@ -60,15 +105,23 @@ while rodando:
     if nova_direcao is not None and nova_direcao != [-direcao[0],-direcao[1]]:
         direcao = nova_direcao       
     cobra,comida,comeu = movimento_cobra(cobra,comida,direcao,tamanhoCobra)
-    colisao = detc_colisao(cobra)
+    colisao = detc_colisao(cobra,Barreiras)
     if colisao == True:
        rodando = False
     if comeu == True:
         pontuacao += 10
+        if dif == 2 or dif == 3:
+            if pontuacao % 100 == 0:
+                nova = adicionar_barreiras(3, cobra, comida, Barreiras, tamanhoCobra)
+                Barreiras.extend(nova)
+
     tela.fill((0,0,0))
     #desenha a pontuacao
     texto = fonte.render(f'Pontuação: {pontuacao}', True, (255,255,255))
     tela.blit(texto, (10, 10))
+    #desenha as barreiras
+    for j in Barreiras:
+        pygame.draw.rect(tela,(210,110,255),(j[0],j[1],tamanhoCobra,tamanhoCobra)) 
     #desenha a comida
     pygame.draw.rect(tela,(0,255,0),(comida[0],comida[1],tamanhoCobra,tamanhoCobra))
    #desenha a cobra
@@ -76,6 +129,6 @@ while rodando:
         pygame.draw.rect(tela,(255,0,0), (i[0],i[1],tamanhoCobra,tamanhoCobra))
     pygame.display.flip()
     #velocidade da cobra, consequentemente do jogo inteiro
-    tempo.tick(10)
+    tempo.tick(velocidade)
 pygame.quit()
 salvar_pontos(nick,pontuacao)
